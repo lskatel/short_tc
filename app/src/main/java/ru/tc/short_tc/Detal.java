0f3ed0e462562187c01676cac0c172d134612bd3
lskatel <krinicynmikhail@gmail.com>
2019-08-28 21:14:44 +0500

    Работал из дома

--- app/src/main/java/ru/tc/short_tc/Detal.java
+++ app/src/main/java/ru/tc/short_tc/Detal.java
@@ -10,23 +10,21 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-public class Main2Activity extends AppCompatActivity {
+public class Detal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_detal);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
     }
 
 }
